612c613b3c5d340ddcc70df6155d92afd1f0dc5a
Andrey Noskov <anoskov@ccsteam.ru>
2016-08-12 18:55:40 +0300

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# aes-ctr-ex
-AES cipher in CTR mode implementation using Elixir
+# AES CTR
+
+AES cipher in CTR mode.
+
+## Installation
+
+If [available in Hex](https://hex.pm/docs/publish), the package can be installed as:
+
+* Add `aes_ctr` to your list of dependencies in `mix.exs`:
+
+```elixir
+def deps do
+  [{:aes_ctr, "~> 0.1.0"]
+end
+```
+
+* Ensure `aes_ctr_ex` is started before your application:
+
+```elixir
+def application do
+  [applications: [:aes_ctr]]
+end
+```
+
+## Usage
+
+* Generate AES_128 key:
+
+```elixir
+{:ok, key} = AesCtr.generate_aes_key(:bytes)
+{:ok, key_in_base64} = AesCtr.generate_aes_key(:base64)
+```
+
+## Configuration
---
+++ mix.exs
@@ -0,0 +1,39 @@
+defmodule AesCtr.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :aes_ctr,
+     version: "0.1.0",
+     elixir: "~> 1.3",
+     build_embedded: Mix.env == :prod,
+     start_permanent: Mix.env == :prod,
+     deps: deps(),
+     aliases: aliases(),
+     description: description(),
+     package: package()]
+  end
+
+  def application do
+    [applications: []]
+  end
+
+  defp deps do
+    []
+  end
+
+  defp aliases do
+    []
+  end
+
+  defp description do
+    "AES cipher in CTR mode."
+  end
+
+  defp package do
+    [name: :aes_ctr,
+     files: ["lib", "mix.exs"],
+     maintainers: ["Andrey Noskov"],
+     licenses: ["MIT"],
+     links: %{"github" => "https://github.com/anoskov/aes-ctr-ex"}]
+  end
+end
